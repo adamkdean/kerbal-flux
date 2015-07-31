866118e0650139097f0c692da1534c78b7d037a2
Adam K Dean <adam@amberfish.co.uk>
2015-07-31 15:33:10 +0100

    Added initial vs solution

---
+++ Solution/KerbalFlux/Plugin/Parts/KFG100FluxGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerbalFlux.Plugin.Parts
+{
+    class KFG100FluxGenerator : PartModule
+    {
+        public override void OnStart(StartState state)
+        {
+            //
+        }
+    }
+}
